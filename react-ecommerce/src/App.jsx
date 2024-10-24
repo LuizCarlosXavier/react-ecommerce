66bb6bd0c54e90437763ca035b2eaee17f83d352
LuizCarlosXavier <luizcarlosabrantes01@gmail.com>
2024-10-23 23:19:41 -0300

    feat: add dynamic routes and pages

--- react-ecommerce/src/App.jsx
+++ react-ecommerce/src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Acessories from './pages/Acessories'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import Men from './pages/Men'
+import Orders from './pages/Orders'
+import PlaceOrders from './pages/PlaceOrders'
+import Product from './pages/Product'
+import Women from './pages/Women'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/acessories' element={<Acessories/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/men' element={<Men/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+        <Route path='/place-orders' element={<PlaceOrders/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/women' element={<Women/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ react-ecommerce/src/pages/Acessories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Acessories = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Acessories
